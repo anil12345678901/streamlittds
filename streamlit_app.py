1583d8300fcb6ef53910850227a9dced52b8f267
ANIL KUMAR BHUKYA <131249940+anil12345678901@users.noreply.github.com>
2023-04-19 18:03:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,11 @@ st.title("TDS Graded Assignment 8")
 a = st.number_input('Insert first number:')
 b = st.number_input('Insert second number:')
 c = st.number_input('Insert third number:')
-largest=st.maxValue(a,b,c)
+largest=0
+if a>=b and a>=c:
+  largest=a
+if b>=c and b>=a:
+  largest=b
+if c>=a and c>=b:
+  largest=c
 st.write(largest)
