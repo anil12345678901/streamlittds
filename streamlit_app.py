1e56e7f84c08c566e1b514976eec4af5d2905241
ANIL KUMAR BHUKYA <131249940+anil12345678901@users.noreply.github.com>
2023-04-19 21:15:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title("TDS Graded Assignment 8")
+st.write("Solution (22f1001422)")
 a = st.number_input('Insert first number:')
 b = st.number_input('Insert second number:')
 c = st.number_input('Insert third number:')
